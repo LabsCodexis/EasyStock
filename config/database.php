58265afb19b593a1709ac644af28aa01752e6aef
labscodexis <labscodexis@gmail.com>
2026-01-27 17:58:31 -0300

    ajuste para deploy 05

--- config/database.php
+++ config/database.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Str;
 
+$rawDbHost = env('DB_HOST', '127.0.0.1');
+$dbHost = is_string($rawDbHost)
+    ? preg_replace('/\\.render\\.com$/', '', $rawDbHost)
+    : $rawDbHost;
+
 return [
 
     /*
@@ -46,7 +51,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $dbHost,
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
@@ -66,7 +71,7 @@ return [
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $dbHost,
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
@@ -86,7 +91,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $dbHost,
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
